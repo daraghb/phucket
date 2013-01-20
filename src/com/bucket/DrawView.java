5f5e2a7f06a94a08800d69aed6a05c22651d6294
Daragh Bland <dbhelterskelter@gmail.com>
2013-01-20 21:24:43 +0000

    test commit

--- src/com/bucket/DrawView.java
+++ src/com/bucket/DrawView.java
@@ -19,7 +19,7 @@ public class DrawView extends View {
         paint.setStrokeWidth(3);
 
         canvas.drawRect(30, 30, 80, 80, paint);
-        //paint.setStrokeWidth(0);
+        paint.setStrokeWidth(0);
         //paint.setColor(Color.CYAN);
         //canvas.drawRect(33, 60, 77, 77, paint );
         //paint.setColor(Color.YELLOW);
